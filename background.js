c2b7c151a903cc9d1153a22003465f1d506bd95a
Samuel Corso <sam@mentat.org>
2018-07-23 19:27:44 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "developer.chrome.com" }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});
