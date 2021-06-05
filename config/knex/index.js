96fc53d9877a86e36584b7233a7564838ffdfd3f
13ROY <leroyheritage@googlemail.com>
2021-06-05 22:34:33 +0100

    Update index.js connectDB function to provide a retries parameter with a default value for recursion purposes

--- config/knex/index.js
+++ config/knex/index.js
@@ -4,14 +4,13 @@ const environment = process.env.NODE_ENV || 'local';
 const configuration = require('../../knexfile')[environment];
 knex = require('knex')(configuration);
 
-function connectToDB() {
-  let retries = 5;
+function connectToDB(retries = 5) {
   if (retries > 0) {
     knex.raw('select 1+1 as result').catch((err) => {
       console.error("Couldn't connect to DB", err);
       retries -= 1;
       console.error(`Retries left: ${retries}`);
-      setTimeout(connectToDB, 5000);
+      setTimeout(connectToDB, 5000, retries);
     });
   }
 }
