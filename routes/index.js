089eb68a8b30e199fe279cecc9501efb102efc67
13ROY <leroyheritage@googlemail.com>
2021-09-16 22:02:14 +0100

    Move validation handler usage to routes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,14 @@ const router = express.Router();
 const addressesRoutes = require('../api/Addresses');
 const proxiesRoutes = require('../api/Proxies');
 const tasksRoutes = require('../api/Tasks');
+const validationHandler = require('../helpers/validation-handler');
 
 router.get('/', (req, res) => res.send('Welcome to the SneakerBot API'));
 router.use('/addresses', addressesRoutes);
 router.use('/proxies', proxiesRoutes);
 router.use('/tasks', tasksRoutes);
+
+// route handlers
+router.use(validationHandler);
 
 module.exports = router;
