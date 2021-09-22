66db55bb1b413fb5de3b62c10e61a2e6a5b69c4e
13ROY <leroyheritage@googlemail.com>
2021-09-23 00:08:47 +0100

    Update knexfile.js to reduce duplication

--- knexfile.js
+++ knexfile.js
@@ -21,27 +21,12 @@ const local = {
 };
 
 const docker = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
-  },
-  useNullAsDefault: true,
-  debug: false,
-  migrations: {
-    directory: 'db/migrations',
-    tableName: 'knex_migrations'
-  },
-  seeds: {
-    directory: 'db/seeds'
-  }
+  ...local,
+  debug: false
 };
+
 module.exports = {
   local,
-  // TODO Add in-memory DB for integration tests
   test: local,
   docker
 };
